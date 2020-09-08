45bca39bf08732e6f1ceaa3edba054e8c165b061
daapx <daapx1@gmail.com>
2020-09-08 00:12:18 -0300

    App con modulos y rutas por modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
 import { AppComponent } from './app.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotpagefoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
